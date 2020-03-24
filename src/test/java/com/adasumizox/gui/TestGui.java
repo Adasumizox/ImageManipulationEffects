ba3d933bbd6938c24c825a7ec65b0e3bd979b9ba
adasumizox <darkdan099@gmail.com>
2020-03-24 23:18:31 +0100

    I merged wrong branch 2

--- src/test/java/com/adasumizox/gui/TestGui.java
+++ src/test/java/com/adasumizox/gui/TestGui.java
@@ -1,4 +1,53 @@
 package com.adasumizox.gui;
 
+import org.assertj.swing.edt.FailOnThreadViolationRepaintManager;
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.fixture.FrameFixture;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class TestGui {
+    private FrameFixture window;
+
+    @BeforeClass
+    public static void setUpOnce() {
+        FailOnThreadViolationRepaintManager.install();
+    }
+
+    @Before
+    public void setUp() {
+        Gui frame = GuiActionRunner.execute(() -> new Gui());
+        window = new FrameFixture(frame);
+        window.show();
+    }
+
+    @Test
+    public void shouldBeAbleToMaximize() {
+        window.maximize();
+    }
+
+    @Test
+    public void shouldBeAbleToRevertMaximize() {
+        window.maximize();
+        window.normalize();
+    }
+
+    @Test
+    public void shouldBeAbleToMinimize() {
+        window.iconify();
+    }
+
+    @Test
+    public void shouldBeAbleToRevertMinimize() {
+        window.iconify();
+        window.deiconify();
+    }
+
+    @After
+    public void tearDown() {
+        window.cleanUp();
+    }
+
 }
