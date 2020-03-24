606bc5f2bb422b2a8b8ab6393df3650c38b56c9c
adasumizox <darkdan099@gmail.com>
2020-03-24 22:52:54 +0100

    I merged wrong branch

--- src/main/java/com/adasumizox/gui/components/GuiFrame.java
+++ src/main/java/com/adasumizox/gui/components/GuiFrame.java
@@ -2,6 +2,14 @@ package com.adasumizox.gui.components;
 
 import javax.swing.JFrame;
 
+// http://www.miglayout.com/mavensite/apidocs/net/miginfocom/layout/ConstraintParser.html
+// A constraint that holds the column or row constraints for the grid. It also holds the gaps between the rows and columns.
+import net.miginfocom.layout.AC;
+// Contains the constraints for an instance of the LC layout manager.
+import net.miginfocom.layout.LC;
+// A very flexible layout manager.
+import net.miginfocom.swing.MigLayout;
+
 public abstract class GuiFrame extends JFrame {
     private static final long serialVersionUID = 1L;
 
@@ -13,5 +21,13 @@ public abstract class GuiFrame extends JFrame {
     public GuiFrame(String name) {
         this();
         setName(name);
+    }
+
+    public void setMiglayout(LC layout, AC columns, AC rows) {
+        setLayout(new MigLayout(layout, columns, rows));
+    }
+
+    public void setMiglayout() {
+        setMiglayout(new LC(), new AC(), new AC());
     }
 }
